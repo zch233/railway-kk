603d086771b25876e95a7be891c6f04fc4ef09d7
张超 <529743595@qq.com>
2022-09-02 11:30:08 +0800

    chore: update lintstaged fix order

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,21 +2,21 @@ module.exports = {
     '*': ['ls-lint'], // 检查文件名称是否规范
     '*.{js,jsx,ts,tsx}': [
         // eslint, prettier 修复
-        'eslint --fix',
         'prettier --write',
+        'eslint --fix',
     ],
     '{!(package)*.json,*.code-snippets,.!(browserslist|nvm|sentry|yarn|npm)*rc}': ['prettier --write --parser json'], // prettier 一些额外格式的文件
     'package.json': ['prettier --write'], // prettier package.json
     '*.vue': [
         // 修复 vue 文件
-        'eslint --fix',
         'prettier --write',
         'stylelint --fix',
+        'eslint --fix',
     ],
     '*.{css,scss,sass,less,styl,html}': [
         // 修复样式文件
-        'stylelint --fix',
         'prettier --write',
+        'stylelint --fix',
     ],
-    '*.md': ['prettier --write'],
+    '*.md': ['prettier --write --parser markdown --prose-wrap never'],
 };
