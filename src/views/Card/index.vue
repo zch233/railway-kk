11f3986048a29999c9abd5a29efef8ecfac3602a
叶俊 <2512636001@qq.com>
2022-10-17 18:00:08 +0800

    feat: add card example

---
+++ src/views/Card/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card container">
+        <GupoCard title="默认卡片">
+            <div>默认内容</div>
+        </GupoCard>
+        <GupoCard title="自定义右侧内容">
+            <template #extra>
+                <a-button type="primary">操作</a-button>
+            </template>
+            <div>默认内容</div>
+        </GupoCard>
+    </div>
+</template>
+
+<script setup>
+import GupoCard from '@src/components/GupoCard/index.vue';
+</script>
+
+<style lang="less" scoped>
+.card {
+    .gupo-card + .gupo-card {
+        margin-top: @space4;
+    }
+}
+</style>
