aaed096f027d4c273824bf740a4d49f890ca6452
张超 <529743595@qq.com>
2022-11-24 10:15:25 +0800

    chore: remove stylelint-less

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     root: true,
-    plugins: ['stylelint-order', 'stylelint-less'],
+    plugins: ['stylelint-order'],
     extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
     customSyntax: 'postcss-html',
     rules: {
