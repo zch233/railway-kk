23d924ae053cc2511c6b890d3c10a0dac18ef982
张超 <529743595@qq.com>
2022-12-01 23:18:13 +0800

    chore: update commit header length limit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
+        'header-max-length': [2, 'always', 200],
         'type-enum': [2, 'always', ['WIP', 'feat', 'fix', 'refactor', 'docs', 'test', 'style', 'perf', 'ci', 'revert', 'chore']],
         'scope-empty': [1, 'never'],
         'subject-case': [0, 'always'],
